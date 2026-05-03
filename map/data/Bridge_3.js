1a4a994f2db225fb2c3f1a5e1a2ad7994b872cfe
Atikur Rahman <55056373+arahmandc@users.noreply.github.com>
2026-05-03 14:36:38 +0600

    map update

---
+++ map/data/Bridge_3.js
@@ -0,0 +1,9 @@
+var json_Bridge_3 = {
+"type": "FeatureCollection",
+"name": "Bridge_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "osm_id": "-622", "name": "301.300m BRIDZE", "highway": "unclassified", "waterway": null, "aerialway": null, "barrier": null, "man_made": null, "z_order": "13", "other_tags": "\"bridge\"=>\"yes\",\"level\"=>\"1\"" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 89.2562529, 25.3128911 ], [ 89.2562771, 25.3129881 ], [ 89.2563763, 25.3133663 ], [ 89.256501, 25.3138379 ], [ 89.2566365, 25.3143592 ], [ 89.2567612, 25.3148296 ], [ 89.2568645, 25.3152503 ], [ 89.2569584, 25.3156152 ], [ 89.2569798, 25.3156976 ] ] ] } },
+{ "type": "Feature", "properties": { "osm_id": "-1085", "name": "39.0m BRIDZE", "highway": "unclassified", "waterway": null, "aerialway": null, "barrier": null, "man_made": null, "z_order": "13", "other_tags": "\"bridge\"=>\"yes\",\"level\"=>\"1\"" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 89.2573647, 25.3168784 ], [ 89.2575377, 25.3174603 ] ] ] } }
+]
+}
